fe99ecb733f23774d0246db678b98c9f6617d050
MauriRiver345 <mrg2735@gmail.com>
2025-03-12 12:56:00 -0600

    feat: :sparkles: Añadi el diseño de las cartas y del header

---
+++ src/components/mainTitle.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export function MainTitle() {
+    return (
+      <div className="main-title">
+        <h1 id='appName'>What Does It Mean?</h1>
+      </div>
+    )
+  }
+  
