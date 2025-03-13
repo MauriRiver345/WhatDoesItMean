3cbd3805177c5bc1f76808a8e8d8967d2e11ab21
MauriRiver345 <mrg2735@gmail.com>
2025-03-13 10:19:02 -0600

    feat:

--- src/components/translateTo.tsx
+++ src/components/translateTo.tsx
@@ -14,7 +14,7 @@ export function TranslateTo(){
             <a href=""><FaExchangeAlt  id='iconExchange'/></a>
         </div>
         <div className="text">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sint qui accusantium ad aut distinctio est fugiat aspernatur natus repudiandae dolore possimus, excepturi modi voluptate aperiam maiores et saepe tenetur.</p>
+            <p>Result Here...</p>
         </div>
         <div className="footer">
             <a href=""><PiSpeakerHighFill /></a>
