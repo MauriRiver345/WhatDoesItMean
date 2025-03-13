cb35f0dc9d019480502e2c2f1ffc92b67de0bb04
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-03-13 12:27:47 -0600

    fix: :bug: Arreglado el fallo de uso del Hook de useTranslate
    Ya se puede usar como una función directamente en lugar de que se active cada vez con useEffect

--- src/hooks/useTranslate.ts
+++ src/hooks/useTranslate.ts
@@ -1,29 +1,22 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 
-export default function useTranslate(text: string, lang: string): string {
+export default function useTranslate() {
   const [translation, setTranslation] = useState<string>("")
 
-  useEffect(() => {
+  async function translate(text: string, lang: string): Promise<string> {
     try {
-      const api = async () => {
-        const url = "https://api.mymemory.translated.net/get"
-        const params = {
-          q: text,
-          langpair: lang,
-        }
-        const queryString = new URLSearchParams(params).toString()
-        const request = new Request(`${url}?${queryString}`)
-
-        const data = await fetch(request)
-        const jsonData = await data.json()
-        const translated = jsonData.responseData.translatedText
-        setTranslation(translated)
-      }
-      api()
+      const url = "https://api.mymemory.translated.net/get"
+      const params = new URLSearchParams({ q: text, langpair: lang })
+      const response = await fetch(`${url}?${params}`)
+      const jsonData = await response.json()
+      const translated = jsonData.responseData.translatedText
+      setTranslation(translated)
+      return translated
     } catch (error) {
       console.error("Error fetching translation:", error)
+      return ""
     }
-  }, [])
-  console.log(translation)
-  return translation
+  }
+
+  return translate
 }
