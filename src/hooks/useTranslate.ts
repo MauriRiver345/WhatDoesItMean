5c0c8cb0087a58deecc77bb56f8a8da28ce311a1
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-03-16 15:27:44 -0600

    fix: :bug: Arreglado el fallo en el render de la traducción
    Enviava un state del resultado sin que se hubiese actualizado, ya mando el resultado de la API directamente

--- src/hooks/useTranslate.ts
+++ src/hooks/useTranslate.ts
@@ -1,17 +1,11 @@
-import { useState } from "react"
-
 export default function useTranslate() {
-  const [translation, setTranslation] = useState<string>("")
-
   async function translate(text: string, lang: string): Promise<string> {
     try {
       const url = "https://api.mymemory.translated.net/get"
       const params = new URLSearchParams({ q: text, langpair: lang })
       const response = await fetch(`${url}?${params}`)
       const jsonData = await response.json()
-      const translated = jsonData.responseData.translatedText
-      setTranslation(translated)
-      return translated
+      return jsonData.responseData.translatedText
     } catch (error) {
       console.error("Error fetching translation:", error)
       return ""
