c1b30de0f72b34a03838c63fd13ff56e184b3009
MauriRiver345 <mrg2735@gmail.com>
2025-03-19 12:41:37 -0600

    feat: :sparkles: ya permite mas idiomas

--- src/components/LeftCard/LeftCard.tsx
+++ src/components/LeftCard/LeftCard.tsx
@@ -1,96 +1,115 @@
-import "./LeftCard.css"
-import { FaCopy } from "react-icons/fa"
-import { PiSpeakerHighFill } from "react-icons/pi"
-import { useTranslationContext } from "../../context/TranslationContext/useTranslationContext"
-import useTranslate from "../../hooks/useTranslate"
-import "../TransalteTo/TranslateTo.css"
-import { FaExchangeAlt } from "react-icons/fa"
-import translate from "../../hooks/useTranslate"
-
+import "./LeftCard.css";
+import { FaCopy } from "react-icons/fa";
+import { PiSpeakerHighFill } from "react-icons/pi";
+import { useTranslationContext } from "../../context/TranslationContext/useTranslationContext";
+import useTranslate from "../../hooks/useTranslate";
+import "../TransalteTo/TranslateTo.css";
+import { FaExchangeAlt } from "react-icons/fa";
+import { useState, useEffect } from "react";
 
 export function LeftCard() {
-  const { setText, text, setLangPair, langPair, setResult } = useTranslationContext()
-  const translate = useTranslate()
+  const { setText, text, setLangPair, langPair, setResult } = useTranslationContext();
+  const translate = useTranslate();
+  
+  // Estado para el idioma seleccionado
+  const [selectedLang, setSelectedLang] = useState<string>(langPair.split("|")[0]);
+
+  useEffect(() => {
+    setSelectedLang(langPair.split("|")[0]); // Actualiza el estado cuando langPair cambia
+  }, [langPair]);
 
   function handleInput(e: React.ChangeEvent<HTMLTextAreaElement>) {
-    e.preventDefault()
+    e.preventDefault();
     if (e.target.value.length > 500) {
-      return
+      return;
     }
-
-    setText(e.target.value)
+    setText(e.target.value);
   }
 
   function changeFromLang(e: React.MouseEvent<HTMLParagraphElement>) {
-    e.preventDefault()
-    const newFromLang = e.currentTarget.dataset.lang
+    e.preventDefault();
+    const newFromLang = e.currentTarget.dataset.lang;
 
     setLangPair((prevState) => {
-      console.log(prevState)
-      const toLang = prevState.split("|")[1]
+      const toLang = prevState.split("|")[1];
+      return `${newFromLang}|${toLang}`;
+    });
+  }
 
-      return `${newFromLang}|${toLang}`
-    })
+  function swapLanguages() {
+    setLangPair((prevState) => {
+      const [fromLang, toLang] = prevState.split("|");
+      return `${toLang}|${fromLang}`;
+    });
   }
 
   async function getTranslation() {
-    const res = await translate(text, langPair)
-    setResult(res)
-
+    const res = await translate(text, langPair);
+    setResult(res);
   }
-
-  console.log(langPair)
 
   return (
     <div className="translateFrom">
       <div className="header">
-        <p>
-          Select Language
-        </p>
-        <p className="Language"
+        <p id="origin">From</p>
+        <p
+          className={`Language ${selectedLang === "es" ? "active" : ""}`}
           onClick={changeFromLang}
           data-lang="es"
         >
           Spanish
         </p>
-        <p className="Language"
-            onClick={changeFromLang}
+        <p
+          className={`Language ${selectedLang === "en" ? "active" : ""}`}
+          onClick={changeFromLang}
           data-lang="en"
         >
           English
         </p>
-        <a href="">
+        <p 
+          className={`Language ${selectedLang === "de" ? "active" : ""}`}
+          onClick={changeFromLang}
+          data-lang="de"
+        >
+          German
+        </p>
+        <p
+          className={`Language ${selectedLang === "it" ? "active" : ""}`}
+          onClick={changeFromLang}
+          data-lang="it"
+        >
+          Italian
+        </p>
+        <a onClick={swapLanguages}>
           <FaExchangeAlt id="iconExchange" />
         </a>
       </div>
       <div className="text">
-      <div className="to-translate-container">
-    <textarea
-      onChange={handleInput}
-      value={text}
-      className="to-translate"
-      placeholder="Type here..."
-    />
-  </div>
-          <div className="total-characters-container">
-    <p className="total-characters">{text.length}/500</p>
-  </div>
+        <div className="to-translate-container">
+          <textarea
+            onChange={handleInput}
+            value={text}
+            className="to-translate"
+            placeholder="Type here..."
+          />
+        </div>
+        <div className="total-characters-container">
+          <p className="total-characters">{text.length}/500</p>
+        </div>
       </div>
       <div className="footerFrom">
-    <div className="iconos">
-    <a href="" className="mini-icons">
-          <PiSpeakerHighFill />
-        </a>
-    <a href="" className="mini-icons">
-          <FaCopy />
-        </a>
-    </div>
-        <button
-      className="translate-button"
-      onClick={getTranslation}>
-      Translate
-    </button>
+        <div className="iconos">
+          <a href="" className="mini-icons">
+            <PiSpeakerHighFill />
+          </a>
+          <a className="mini-icons" onClick={() => navigator.clipboard.writeText(text)}>
+            <FaCopy />
+          </a>
+        </div>
+        <button className="translate-button" onClick={getTranslation}>
+          Translate
+        </button>
       </div>
     </div>
-  )
+  );
 }
--- src/components/TransalteTo/TranslateTo.tsx
+++ src/components/TransalteTo/TranslateTo.tsx
@@ -1,59 +1,76 @@
-import "./translateTo.css"
-import { FaExchangeAlt } from "react-icons/fa"
-import { PiSpeakerHighFill } from "react-icons/pi"
-import { FaCopy } from "react-icons/fa"
-import { useTranslationContext } from "../../context/TranslationContext/useTranslationContext"
-import useTranslate from "../../hooks/useTranslate"
+import "./translateTo.css";
+import { FaExchangeAlt } from "react-icons/fa";
+import { PiSpeakerHighFill } from "react-icons/pi";
+import { FaCopy } from "react-icons/fa";
+import { useTranslationContext } from "../../context/TranslationContext/useTranslationContext";
+import { useState, useEffect } from "react";
 
 export function TranslateTo() {
-  const { text, langPair, setLangPair, result } = useTranslationContext()
+  const { langPair, setLangPair, result } = useTranslationContext();
+  
+  // Estado para el idioma de destino seleccionado
+  const [selectedLang, setSelectedLang] = useState<string>(langPair.split("|")[1]);
+
+  // Efecto para actualizar el idioma activo cuando cambie `langPair`
+  useEffect(() => {
+    setSelectedLang(langPair.split("|")[1]);
+  }, [langPair]);
 
   function changeToLang(e: React.MouseEvent<HTMLParagraphElement>) {
-    e.preventDefault()
-    const newToLang = e.currentTarget.dataset.lang
+    e.preventDefault();
+    const newToLang = e.currentTarget.dataset.lang;
 
     setLangPair((prevState) => {
-      console.log(prevState)
-      const fromLang = prevState.split("|")[0]
-
-      return `${fromLang}|${newToLang}`
-    })
+      const fromLang = prevState.split("|")[0];
+      return `${fromLang}|${newToLang}`;
+    });
   }
-
-  console.log(langPair)
 
   return (
     <div className="translateTo">
       <div className="header">
-        <p className="Language"
+        <p id="Destiny">To</p>
+        <p
+          className={`Language ${selectedLang === "en" ? "active" : ""}`}
           onClick={changeToLang}
           data-lang="en"
         >
           English
         </p>
-        <p className="Language"
+        <p
+          className={`Language ${selectedLang === "es" ? "active" : ""}`}
           onClick={changeToLang}
           data-lang="es"
         >
           Spanish
         </p>
-        <a href="">
-          <FaExchangeAlt id="iconExchange" />
-        </a>
+        <p
+          className={`Language ${selectedLang === "de" ? "active" : ""}`}
+          onClick={changeToLang}
+          data-lang="de"
+        >
+          German
+        </p>
+        <p
+          className={`Language ${selectedLang === "it" ? "active" : ""}`}
+          onClick={changeToLang}
+          data-lang="it"
+        >
+          Italian
+        </p>
       </div>
       <div className="text">
         <p className="result">{result}</p>
       </div>
       <div className="footer">
-        <div className="iconos"> 
-        </div>
+        <div className="iconos"></div>
         <a href="" className="mini-icons">
           <PiSpeakerHighFill />
         </a>
-        <a href="" className="mini-icons">
+        <a className="mini-icons" onClick={() => navigator.clipboard.writeText(result)}>
           <FaCopy />
         </a>
       </div>
     </div>
-  )
+  );
 }
